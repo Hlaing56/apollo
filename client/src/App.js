9ab31a02cfdda621db98ca3ea1ddf1b7e5f9436c
Hlaing Aung <betahba@gmail.com>
2022-03-08 20:56:16 -0500

    added countdown

--- client/src/App.js
+++ client/src/App.js
@@ -57,4 +57,30 @@ function App() {
   );
 }
 
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const currentMonth = new Date().getMonth();
+
+const newMonth = new Date(` ${currentMonth + 2} 1 2022 00:00:00`);
+
+function countdownTimer() {
+    const currentTime = new Date();
+    const diff = newMonth - currentTime;
+    
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+
+    days.innerHTML = d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+};
+
+setInterval(countdownTimer, 1000);
+
 export default App;
--- client/src/pages/Leaderboard.js
+++ client/src/pages/Leaderboard.js
@@ -14,6 +14,24 @@ const Leaderboard = () => {
     <main>
       <div className="flex-box justify-space-between">
         <div className="col-12 mb-3">
+        <div id="countdown" class="countdown">
+            <div class="time">
+                <h2 id="days">00</h2>
+                <small>Days</small>
+            </div>
+            <div class="time">
+                <h2 id="hours">00</h2>
+                <small>Hours</small>
+            </div>
+            <div class="time">
+                <h2 id="minutes">00</h2>
+                <small>Minutes</small>
+            </div>
+            <div class="time">
+                <h2 id="seconds">00</h2>
+                <small>Seconds</small>
+            </div>
+        </div>
           {loading ? (
             <div>Loading...</div>
           ) : (
